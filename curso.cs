a381e41c546123f5d6b9db233eb9bd76c26eb8c4
mikaelisaac <mikaelcisaac@gmail.com>
2023-01-13 14:00:22 -0300

    Update curso.cs

--- curso.cs
+++ curso.cs
@@ -172,4 +172,102 @@
     decimal celsius = (fahrenheit - 32m) * (5m / 9m);
     Console.WriteLine("The temperature is " + celsius + " Celsius.");
 
-// 
+// Métodos
+    // Dado D7
+    Random dice = new Random();
+    int roll = dice.Next(1, 7);
+    Console.WriteLine(roll);
+
+    
+    // Método sobrecaregado é quando pode-se passar diversos tipos de valores por parâmetro
+    Random dice = new Random();
+    int roll1 = dice.Next(); // Sem limite
+    int roll2 = dice.Next(101); // Limite superior
+    int roll3 = dice.Next(50, 101); // Intervalo de valores
+
+    Console.WriteLine($"First roll: {roll1}");
+    Console.WriteLine($"Second roll: {roll2}");
+    Console.WriteLine($"Third roll: {roll3}");
+
+// Desafio
+    // Achar o maior valor entre as variáveis
+    int firstValue = 500;
+    int secondValue = 600;
+    int largerValue;
+    largerValue = Math.Max(firstValue, secondValue);
+    Console.WriteLine(largerValue);
+
+// Condições
+    Random dice = new Random();
+
+    int roll1 = dice.Next(1, 7);
+    int roll2 = dice.Next(1, 7);
+    int roll3 = dice.Next(1, 7);
+
+    int total = roll1 + roll2 + roll3;
+
+    Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+    if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+    {
+        if ((roll1 == roll2) && (roll2 == roll3))
+        {
+            Console.WriteLine("You rolled triples!  +6 bonus to total!");
+            total += 6;
+        }
+        else
+        {
+            Console.WriteLine("You rolled doubles!  +2 bonus to total!");
+            total += 2;
+        }
+    }
+
+    if (total >= 16)
+    {
+        Console.WriteLine("You win a new car!");
+    }
+    else if (total >= 10)
+    {
+        Console.WriteLine("You win a new laptop!");
+    }
+    else if (total == 7)
+    {
+        Console.WriteLine("You win a trip for two!");
+    }
+    else
+    {
+        Console.WriteLine("You win a kitten!");
+    }
+// Matrizes
+    /*
+    string[] fraudulentOrderIDs = new string[3];
+
+    fraudulentOrderIDs[0] = "A123";
+    fraudulentOrderIDs[1] = "B456";
+    fraudulentOrderIDs[2] = "C789";
+    // fraudulentOrderIDs[3] = "D000";
+    */
+
+    string[] fraudulentOrderIDs = { "A123", "B456", "C789" };
+
+    Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
+    Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
+    Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
+
+    fraudulentOrderIDs[0] = "F000";
+
+    Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
+
+    Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process.");
+
+    // For each na matriz, com contador e acumulador
+    int[] inventory = { 200, 450, 700, 175, 250 };
+    int sum = 0;
+    int bin = 0;
+    foreach (int items in inventory)
+    {
+        sum += items;
+        bin++;
+        Console.WriteLine($"Bin {bin} = {items} items (Running total: {sum})");
+    }
+    Console.WriteLine($"We have {sum} items in inventory.");
